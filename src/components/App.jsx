2c950c37e79c4fcf9449f62e23dd1d80468ca7dd
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-05-04 23:43:38 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    image: null,
+  };
+
+  componentDidMount() {
+    fetch(
+      'https://pixabay.com/api/?q=cat&page=1&key=35916807-4b4569ea51a2cf76aabe9d625&image_type=photo&orientation=horizontal&per_page=12'
+    )
+      .then(res => res.json())
+      .then(image => this.setState({ image }));
+  }
+
+  render() {
+    return <div>{this.state.image && <div>тут картинка</div>}</div>;
+  }
+}
+
+export default App;
